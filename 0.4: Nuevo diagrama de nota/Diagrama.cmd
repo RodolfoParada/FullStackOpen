866bfc8be2e4d8ddf12e10fdc2b53548bf8c76d0
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2024-10-18 22:44:54 -0300

    Update Diagrama.cmd

--- 0.4: Nuevo diagrama de nota/Diagrama.cmd
+++ 0.4: Nuevo diagrama de nota/Diagrama.cmd
@@ -10,18 +10,7 @@ secuenciaDiagrama
     usuario->>Botón:  Al finalizar, el usuario hace clic en el botón **Enviar**.
     usuario->>Proceso Exitoso: Después de un procesamiento exitoso, el post aparece en la lista de publicaciones visibles en la web..
     
-   
+ finDiagrama  
+ secuenciaDiagrama
 
-    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/main.js
-    activate server
-    server-->>browser: the JavaScript file
-    deactivate server
-
-    Note right of browser: The browser starts executing the JavaScript code that fetches the JSON from the server
-
-    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/data.json
-    activate server
-    server-->>browser: [{ "content": "HTML is easy", "date": "2023-1-1" }, ... ]
-    deactivate server
-
-    Note right of browser: The browser executes the callback function that renders the notes
+ 
